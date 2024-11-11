e7093d8a7948ae8d805baac79de8ccb6c4f0374e
PUBKABOZKAX <g_churakov@list.ru>
2024-11-12 00:54:05 +0300

    add ShamanArmor

---
+++ src/main/java/me/udnek/rpgu/item/equipment/shaman/ShamanArmor.java
@@ -0,0 +1,38 @@
+package me.udnek.rpgu.item.equipment.shaman;
+
+import me.udnek.itemscoreu.customattribute.CustomAttribute;
+import me.udnek.itemscoreu.customequipmentslot.CustomEquipmentSlot;
+import me.udnek.itemscoreu.customitem.ConstructableCustomItem;
+import me.udnek.itemscoreu.util.LoreBuilder;
+import me.udnek.rpgu.RpgU;
+import me.udnek.rpgu.lore.AttributesLorePart;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.components.EquippableComponent;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class ShamanArmor extends ConstructableCustomItem {
+
+    @Override
+    public ItemFlag[] getTooltipHides() {return new ItemFlag[]{ItemFlag.HIDE_ATTRIBUTES};}
+
+    @Override
+    public @Nullable EquippableComponent getEquippable() {
+        EquippableComponent equippable = new ItemStack(getMaterial()).getItemMeta().getEquippable();
+        equippable.setSlot(getMaterial().getEquipmentSlot());
+        equippable.setModel(new NamespacedKey(RpgU.getInstance(), "shaman"));
+        return equippable;
+    }
+
+    public @Nullable LoreBuilder getLoreBuilder(CustomEquipmentSlot slot) {
+        LoreBuilder loreBuilder = new LoreBuilder();
+        AttributesLorePart attributesLorePart = new AttributesLorePart();
+        loreBuilder.set(LoreBuilder.Position.ATTRIBUTES, attributesLorePart);
+        attributesLorePart.addAttribute(slot, Component.translatable(getRawItemName()+".description.0").color(CustomAttribute.PLUS_COLOR));
+        attributesLorePart.addAttribute(slot, Component.translatable(getRawItemName()+".description.1").color(NamedTextColor.GRAY));
+        return loreBuilder;
+    }
+}
