41415aed8dc984d8d771af99b24c1657ba84d281
PUBKABOZKAX <g_churakov@list.ru>
2025-12-25 00:11:58 +0300

    to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     kotlin("jvm") version "2.1.20"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "9.3.0"
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
 }
 
@@ -25,5 +27,20 @@ dependencies {
     // Системные зависимости из pom.xml (CoreU и JeiU)
     compileOnly(files("H:/test/plugins/ItemsCoreU-1.0-SNAPSHOT-remapped.jar"))
     compileOnly(files("H:/test/plugins/JeiU.jar"))
+
     implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        compilerOptions.jvmTarget.set(JvmTarget.JVM_21)
+    }
+    compileJava {
+        options.release.set(21)
+    }
+    shadowJar {
+        archiveBaseName.set("RpgU")
+        archiveClassifier.set("")
+        archiveVersion.set(version.toString())
+    }
 }
