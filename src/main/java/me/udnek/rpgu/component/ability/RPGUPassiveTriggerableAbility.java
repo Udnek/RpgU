4822e0e8909d669e2974b68c50746fd9d13aab96
PUBKABOZKAX <g_churakov@list.ru>
2026-01-09 21:03:12 +0300

    folded map, Make crossbow iron again, Make shield iron again

--- src/main/java/me/udnek/rpgu/component/ability/RPGUPassiveTriggerableAbility.java
+++ src/main/java/me/udnek/rpgu/component/ability/RPGUPassiveTriggerableAbility.java
@@ -4,6 +4,7 @@ import me.udnek.coreu.custom.equipment.universal.UniversalInventorySlot;
 import me.udnek.coreu.custom.item.CustomItem;
 import me.udnek.coreu.rpgu.component.ability.passive.RPGUItemPassiveAbility;
 import me.udnek.rpgu.mechanic.damaging.DamageEvent;
+import org.bukkit.event.entity.EntityResurrectEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.entity.EntityToggleGlideEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -16,4 +17,5 @@ public interface RPGUPassiveTriggerableAbility<Context> extends RPGUItemPassiveA
     default void onDamageDealt(@NotNull CustomItem customItem, @NotNull UniversalInventorySlot slot, @NotNull DamageEvent event) {}
     default void onToggleGlide(@NotNull CustomItem customItem, @NotNull UniversalInventorySlot slot, @NotNull EntityToggleGlideEvent event) {}
     default void onDeath(@NotNull CustomItem customItem, @NotNull UniversalInventorySlot slot, @NotNull PlayerDeathEvent event) {}
+    default void onResurrect(@NotNull CustomItem customItem, @NotNull UniversalInventorySlot slot, boolean activatedBefore, @NotNull EntityResurrectEvent event) {}
 }
